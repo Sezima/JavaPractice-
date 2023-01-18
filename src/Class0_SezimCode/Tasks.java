a742bb385edc7bddab94caf3534ee9e3712a5847
sezim <osmsezimka@gmail.com>
2023-01-19 03:27:27 +0600

    Solve other task on website

---
+++ src/Class0_SezimCode/Tasks.java
@@ -0,0 +1,158 @@
+package Class0_SezimCode;
+
+import java.util.Scanner;
+
+public class Tasks {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        /*
+
+//        20.12.2022
+//        1
+        System.out.println("Hello");
+        System.out.println("Alexandra Abramov");
+
+
+//        2; 3
+        System.out.println(74+36);
+        System.out.println(50/3);
+
+
+//        4
+        System.out.println(-5 + 8 * 6);
+        System.out.println((55 + 9 ) % 9);
+        System.out.println(20 + 3 * 5 / 8);
+        System.out.println(5 + 15 /3 * 2 - 8 % 3 );
+
+//        5
+
+        System.out.print("Input first number: ");
+        int num1 = input.nextInt();
+        System.out.print("Input second number: ");
+        int num2 = input.nextInt();
+        System.out.println(num1 + "+" + num2 + "=" + num1 * num2);
+*/
+
+
+        /*
+//        6
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input first number: ");
+        int num1 = input.nextInt();
+        System.out.print("Input second number: ");
+        int num2 = input.nextInt();
+        System.out.println(num1 + "+" + num2 + "=" + (num1 + num2));
+        System.out.println(num1 - num2);
+        System.out.println(num1 + "*" + num2 + "=" + num1 * num2);
+        System.out.println(num1 + "/" + num2 + "=" + num1 / num2);
+        System.out.println(num1 + "%" + num2 + "=" + num1 % num2);
+
+
+
+//        7
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input first number: ");
+        int num1 = input.nextInt();
+        int a = 0;
+
+        while (a < 10) {
+            a++;
+            System.out.println(a);
+            System.out.println(num1 * a);
+
+
+        }
+
+
+
+//        8
+        System.out.println("   J    a   v     v   a");
+        System.out.println("   J   a a   v   v   a a");
+        System.out.println("J  J  aaaaa   v v   aaaaa ");
+        System.out.println(" JJ  a      a  v   a     a ");
+
+
+
+
+
+//        9; 10
+
+        System.out.println(((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)));
+        System.out.println(4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)));
+
+
+//        11
+        System.out.print("Radius: ");
+        float a = input.nextFloat();
+        System.out.println("Perimeter: " + 2 * (a * 3.14));
+        System.out.println("Area is: " + 3.14 * (a * a));
+
+//        12
+        int a1 = input.nextInt();
+        int a2 = input.nextInt();
+        int a3 = input.nextInt();
+        System.out.println((a1 + a2 + a3) / 3);
+
+
+
+//        13
+
+
+        System.out.print("Width: ");
+        float width = input.nextFloat();
+        System.out.print("Height: ");
+        float height = input.nextFloat();
+        System.out.println("Area is: " + (width * height));
+        System.out.println("Perimeter is: " + (2 * (width + height)));
+
+
+
+
+
+//        15
+        System.out.println("1: ");
+        int n1 = input.nextInt();
+        System.out.println("2: ");
+        int n2 = input.nextInt();
+        int c = n1;
+        n1 = n2;
+        n2 = c;
+        System.out.println(n1);
+        System.out.println(n2);
+
+
+
+//        16
+
+        System.out.println(" +'''''+ ");
+        System.out.println(" +\"\"\"\"\"+ ");
+        System.out.println("[| 0 0 |]");
+        System.out.println(" |  ^  |");
+        System.out.println(" | '_' |");
+        System.out.println(" +-----+");
+
+*/
+
+        //        14
+
+
+
+//        21.12.2022
+//        17
+//        System.out.print("Input first number: ");
+//        int a = input.nextInt();
+//        System.out.print("Input second number: ");
+//        int b = input.nextInt();
+//        System.out.println("Product of two binary numbers: " + a * b);
+
+
+//        20
+        System.out.print("Input decimal number: ");
+        int decimalNum = input.nextInt();
+        if (decimalNum == 0){
+            System.out.println();
+        }
+
+    }
+}
