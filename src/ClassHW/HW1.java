56bb5c84df5f65ea962d1780584fc214d66dc5b7
sezim <osmsezimka@gmail.com>
2023-01-19 03:12:13 +0600

    HomeWork1

---
+++ src/ClassHW/HW1.java
@@ -0,0 +1,26 @@
+package ClassHW;
+
+public class HW1 {
+    public static void main(String[] args) {
+        byte byteNum = -128;
+        short s = 5622;
+        int num = 5465664;
+        long l = -656665;
+        float f = 2.890025f;
+        double d = -0.5656;
+        String name = "Sezim";
+        char _ = '*';
+        boolean like = true;
+
+        System.out.println("byte: " + byteNum);
+        System.out.println("short: " + s);
+        System.out.println("int: " + num);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("name: " + name);
+        System.out.println("Symbol: " + _);
+        System.out.println("Boolean: " + like);
+
+    }
+}
