4341164fb879c181d329a437785e8e6d0f8e92c2
sezim <osmsezimka@gmail.com>
2023-01-19 03:30:54 +0600

    Sezm_Code practice with Youtube 'loop'

---
+++ src/Class0_SezimCode/loop.java
@@ -0,0 +1,37 @@
+package Class0_SezimCode;
+
+public class loop {
+    public static void main(String[] args) {
+
+
+        //loop for
+
+//        for (int i = 100; i > 10; i /= 2) {
+//            System.out.println("Element: " + i);
+//        }
+
+
+
+
+
+
+//        loop while
+
+//        int i = 0;
+//        while (i < 10) {
+//            System.out.println(i);
+//            i++;
+//        }
+
+
+
+
+//        loop DO While
+        int i = 100;
+        do {
+            System.out.println(i);
+            i *= 100;
+
+        } while (i < 10);
+    }
+}
