995198c3fe14e6f5deb5cbdbf7c3c52e078be6af
sezim <osmsezimka@gmail.com>
2023-01-19 03:10:44 +0600

    This task about Data Type

---
+++ src/Class1/DataType.java
@@ -0,0 +1,49 @@
+package Class1;
+
+public class DataType {
+    public static void main(String[] args) {
+        //       int num = 20;
+        //
+        //        System.out.println(num++);
+        //        System.out.println(num);
+        //
+        //        System.out.println("===================");
+        //        System.out.println(++num);
+        //        System.out.println(num);
+
+        int creditScore = 16000;
+        boolean marital = false;
+        int childrenCount = 0;
+        boolean giveCoverage = creditScore < 0 && childrenCount == 0 && marital == false;
+        System.out.println(giveCoverage);
+
+
+
+
+
+
+
+         /*
+        System.out.println("I'm studying in DevX");
+        System.out.println("I'm from Kyrygyzstan, I'm 18 y.o");
+        System.out.println(" I come from a small family only 3 members(my parents and me)");
+        System.out.println("I graduated from school in 2021");
+        System.out.println("Bye");
+         */
+
+
+    /*
+          String name = "Sezim";
+          char gender = 'F';
+          boolean question = false;
+
+          System.out.println(name);
+          System.out.println(gender);
+          System.out.println(question);
+    */
+
+
+
+
+    }
+}
